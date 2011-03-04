a153e0524c2adbc309574032d4d1ecbb51f15373
bob <bob@Odin.(none)>
2011-03-03 21:27:28 -0800

    Implemented stack functionality.

--- src/main.c
+++ src/main.c
@@ -4,5 +4,16 @@
 #include "fib.h"
 
 int main(void) {
+	int choice;
+
+	do {
+		printf("Enter value: ");
+		scanf("%d", &choice);
+
+		if (choice > 1) push(choice);
+		else printf("value on top is %d\n", pop());
+
+	} while(choice != -1);
+
 	return 0;
 }
--- src/stack.c
+++ src/stack.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define SIZE 50
+
+int stack[SIZE], *tos = stack, *p1 = stack;
+
 void push(int i) {
-	printf("push(%d): unimplemented\n", i);
+	p1++;
+	if(p1 == (tos+SIZE)) {
+		printf("Stack Overflow.\n");
+		exit(1);
+	}
+	*p1 = i;
 }
 
 int pop(void) {
-	printf("pop: unimplemented\n");
-	return 0;
+	if(p1 == tos) {
+		printf("Stack Underflow.\n");
+		exit(1);
+	}
+	p1--;
+	return *(p1+1);
 }
 
 int peek(void) {
-	printf("peek: unimplemented\n");
-	return 0;
+	return *p1;
 }
-
